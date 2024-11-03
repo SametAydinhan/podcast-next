eacd1f14fa3fba45938cedc5a79127806dcf06e9
Samet Aydinhan <samet.aydinhan879@gmail.com>
2024-11-03 16:53:25 +0300

    feat(leftside): left nav create

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,6 @@
 import LeftSidebar from "@/components/LeftSidebar";
 import type { Metadata } from "next";
+import Image from "next/image";
 
 export default function RootLayout({
   children,
@@ -7,9 +8,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div>
-        <main>
+    <div className="relative flex flex-col">
+        <main className="relative flex bg-black-3">
             <LeftSidebar />
+            <section>
+                <div>
+                    <div>
+                        <Image />
+                        MobileNav
+                    </div>
+                    <div>
+                        Toaster
+                    </div>
+                </div>
+            </section>
             {children}
             <p className="text-white-1">RIGHT SIDEBAR</p>
         </main>
